58106fe40ff984e35b502a3bee9217e59a96addb
kenan10 <denis2014grom@gmail.com>
2025-01-25 00:22:32 +0100

    feature: landing page UI implementation

--- unicore/src/app/layout.tsx
+++ unicore/src/app/layout.tsx
@@ -2,20 +2,28 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/HeaderComponent";
 import Footer from "@/components/FooterComponent";
+import { Roboto } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "UNICORE",
   description: "Meke Ed Easy",
 };
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
+
 export default function RootLayout({
   children,
-}:{
+}: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="bg-silver">
+      <body className={`${roboto.variable} bg-silver font-roboto`}>
         <Header />
         <main>{children}</main>
         <Footer />
---
+++ unicore/src/sections/home-page/KeyFeatures.tsx
@@ -0,0 +1,53 @@
+const features = [
+  {
+    title: "Digital Integration",
+    description:
+      "Simplify your university life with document signing, booking, and more",
+    iconText: "📄",
+  },
+  {
+    title: "Effortless Communication",
+    description: "Chat with peers, faculty, and support staff",
+    iconText: "💬",
+  },
+  {
+    title: "Streamlined Schedules",
+    description: "Access your classes and deadlines in one place",
+    iconText: "📅",
+  },
+];
+
+const KeyFeatures = () => {
+  return (
+    <section className="py-12 ld:py-0 w-full md:min-h-[30vh] lg:min-h-[100vh] flex flex-col lg:flex-row items-center justify-between">
+      {/* Left Section */}
+      <div className="md:w-1/2 text-left">
+        <h2 className="text-2xl font-bold">Key Features Overview</h2>
+        <p className="mt-2">Discover the benefits of using UniCore</p>
+      </div>
+
+      {/* Right Section */}
+      <div className="w-full flex flex-col space-y-4 md:h-[480px] max-w-[520px]">
+        {features.map((feature, index) => (
+          <div
+            key={index}
+            className="flex items-center h-full w-full shadow-sm border-gray-100 border-2 rounded-lg p-4 hover:shadow-lg transition md:gap-11 "
+          >
+            {/* Icon Box */}
+            <div className="w-12 h-12 flex items-center justify-center bg-gray-200 rounded-md text-2xl">
+              {feature.iconText}
+            </div>
+
+            {/* Text Content */}
+            <div className="ml-4">
+              <p className="font-bold font-xl">{feature.title}</p>
+              <p>{feature.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default KeyFeatures;
