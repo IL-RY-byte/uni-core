d9fc9d7c280daf4f6fadf454e34094b97af2c550
kenan10 <denis2014grom@gmail.com>
2025-02-23 20:52:06 +0100

    fix: disable ssr for map

--- unicore/src/app/dash/map/page.tsx
+++ unicore/src/app/dash/map/page.tsx
@@ -1,4 +1,8 @@
-import CampusMap from "@/sections/map-page/CampusMap";
+import dynamic from "next/dynamic";
+
+const CampusMap = dynamic(() => import("@/sections/map-page/CampusMap"), {
+  ssr: false, 
+});
 
 export default function MapPage() {
   return <CampusMap />;
