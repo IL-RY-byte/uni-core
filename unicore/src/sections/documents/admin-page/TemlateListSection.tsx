7ade48872219796f7c3e5b5a1477e9938b2c1c1e
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2025-02-28 16:43:35 +0200

    chore: documents aple page structure

---
+++ unicore/src/sections/documents/admin-page/TemlateListSection.tsx
@@ -0,0 +1,19 @@
+// template id
+// service
+// type
+// secription
+// way persona
+// action
+
+const TemplateListSection = () => {
+    return (
+      <section className="container bg-silver">
+        <div>
+            <p>main</p>
+        </div>
+      </section>
+    );
+  }
+  
+  export default TemplateListSection;
+  
